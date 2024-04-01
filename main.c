d602963929e07b51faccf5c23e6beee74a62102f
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-04-01 20:02:38 +0300

    recursion 1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int summ=0;
+int sum(int a)
+{
+    if(a!=3)
+         sum(a+1);
+    summ+=a;
+return summ;
+}
+
+
+int main() {
+    // Write C code here
+    int a=1;
+    int result= sum(a);
+    printf("%d",result);
+    return 0;
+}
